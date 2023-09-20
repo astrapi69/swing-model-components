89a62637f7e73bf08282b64cc162f01b6205190c
astrapi69 <asterios.raptis@gmx.net>
2023-09-20 17:28:01 +0200

    initial version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module swing.model.components.main
+{
+	requires java.desktop;
+	requires model.data;
+	requires lombok;
+	requires menu.actions.main;
+	requires throwable;
+	requires swing.base.components.main;
+}
